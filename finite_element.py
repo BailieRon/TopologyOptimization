baf825608df5adc6e2865ba9e1344fbe2c2e8d03
ronbailie <ronald.bailie@colorado.edu>
2024-05-04 12:24:45 -0600

    playing with boundary conditions

--- finite_element.py
+++ finite_element.py
@@ -25,14 +25,30 @@ def FE(nelx, nely, x, penal, lk):
                 ]
             )
             K[np.ix_(edof - 1, edof - 1)] += x[ely - 1, elx - 1] ** penal * KE
-    
     F[1, 0] = -1
+    # top_mid_node = (nelx // 4) + 1
+    # F[top_mid_node] = 10
+
+    # lower_left_node = 2 * [(nely + 1) * (nelx + 1) + 1
+    # lower_right_node = (nelx + 2) * (nely + 1) - 1
+    # dof_fixed = np.array([
+    #     2 * lower_left_node, 2 * lower_left_node + 1,
+    #     2 * lower_right_node, 2 * lower_right_node + 1
+    # ])
+    
+    # lower_left_node_index = nely + 1 * (nelx + 1) + 1 # First node of the last row
+    # lower_right_node_index = (nelx + 1) * (nely + 1) - 1  # Last node of the last row
+    # dof_fixed = np.array([
+    #     2 * lower_left_node_index, 2 * lower_left_node_index + 1,
+    #     2 * lower_right_node_index, 2 * lower_right_node_index + 1
+    # ])
 
     # loads and supports
     # identify geometrically constrained nodes from element x and y arrays
     dof_fixed = np.union1d(
         np.arange(0, 2 * (nely + 1), 2), np.array([2 * (nelx + 1) * (nely + 1) - 1])
     )
+
     # array of nodes from element x and y arrays
     dofs = np.arange(0, 2 * (nelx + 1) * (nely + 1))
     # filter mask to grab free nodes from node list
@@ -43,4 +59,5 @@ def FE(nelx, nely, x, penal, lk):
         K[np.ix_(dof_free, dof_free)], F[dof_free]
     )  # solve for displacement at free nodes
     U[dof_fixed] = 0  # fix geometrically constrained nodes
+    
     return U
