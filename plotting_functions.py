014305823e914f78845d125b14002d5e7cf104aa
ronbailie <ronald.bailie@colorado.edu>
2024-05-03 21:20:29 -0600

    add element scatter plot (WIP)

--- plotting_functions.py
+++ plotting_functions.py
@@ -9,3 +9,13 @@ def convergencePlot(c_hist):
     plt.ylabel("Objective Function Value")
     plt.grid(True)
     plt.show()
+    
+
+# plt.figure()
+# X, Y = np.meshgrid(range(nelx+1), range(nely+1))
+# plt.pcolormesh(X, Y, x, cmap='gray', edgecolor='k', shading='flat')
+# plt.scatter(X.ravel()[dof_fixed//2 % (nelx+1)], Y.ravel()[dof_fixed//2 // (nelx+1)], color='red', s=50)
+# plt.scatter([0], [0], color='green', s=50) # force is applied at the top left
+# plt.gca().set_aspect('equal', adjustable='box')
+# plt.colorbar()
+# plt.show()
